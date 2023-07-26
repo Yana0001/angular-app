084d618127c5d10db297cb1b81dd57212ad57161
Yana0001 <119385481+Yana0001@users.noreply.github.com>
2023-07-26 12:47:18 +0300

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from "./server/server.component";
+import { PracticeComponent } from "./practice/practice.component";
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    PracticeComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule

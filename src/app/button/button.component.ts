b82dc90b38f6a46b468c1b585f78af09c0c9a0aa
Yana0001 <119385481+Yana0001@users.noreply.github.com>
2023-10-15 18:21:24 +0300

    ngFor, ngStyle, ngClass, toggle, array practice

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -2,11 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'btn-comp',
-    templateUrl:'./button.component.html'
+    templateUrl: './button.component.html',
+    styleUrls: ['./button.component.css']
 })
 
 export class ButtonComponent {
     showDetails = false;
+    log = [];
+
+
     constructor() {
+    }
+
+    onToggleDetails() {
+        this.showDetails = !this.showDetails;
+        this.log.push(this.log.length + 1);
     }
 }
